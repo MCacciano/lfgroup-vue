e835335e7f40dd6be4b661ab80e1034bb5bef936
Michael Cacciano <michael.cacciano@gmail.com>
2021-01-08 15:33:59 -0500

    initial commit, cleaned up files, added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './public/**/*.html', './src/**/*.vue'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
